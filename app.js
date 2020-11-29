8d1009b474b87fe913eb1ab3c0d30d63fff46520
voyevoda <elisa9el@gmail.com>
2020-11-29 18:06:24 -0500

    Added new Platform class to create the platforms

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
     let isGameOver = false;
     let platformCount = 5;
 
+    class Platform {
+        constructor(newPlatBottom) {
+            this.bottom = newPlatBottom;
+            this.left = Math.random() * 315;
+            this.visual = document.createElement('div');
+
+            const visual = this.visual;
+            visual.classList.add('platform');
+            visual.style.left = `${this.left}px`;
+            visual.style.bottom = `${this.bottom}px`;
+            grid.appendChild(visual);
+        }
+    }
+
     function createPlayer() {
         grid.appendChild(player);
         player.classList.add('player');
@@ -17,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
         for(let i = 0; i < platformCount; i++) {
             let platGap = 600 / platformCount;
             let newPlatBottom = 100 + i * platGap;
-            let newPlatform = new Platform();
+            let newPlatform = new Platform(newPlatBottom);
         }
     }
 
