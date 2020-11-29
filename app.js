11818774178ae18e7b540b9ab87be27a296c3186
voyevoda <elisa9el@gmail.com>
2020-11-29 17:05:45 -0500

    Moved the player using js styles

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
     const player = document.createElement('div');
+    let playerLeftSpace = 50;
+    let playerBottomSpace = 150;
 
     function createPlayer() {
         grid.appendChild(player);
         player.classList.add('player');
+        player.style.left = `${playerLeftSpace}px`;
+        player.style.bottom = `${playerBottomSpace}px`;
     }
 
     createPlayer();
